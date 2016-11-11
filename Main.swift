62ffd61b223c2bd3e99d1dfc5420dce79f1e92a3
Guilherme Siani <guilherme@siani.com.br>
2016-11-11 00:01:39 -0200

    inserted parameter to talk batman function

--- Main.swift
+++ Main.swift
@@ -3,9 +3,9 @@ class Batman
 	var life = 100;
     var armor = 0;
    
-    func talk() -> String
+    func talk(by phrase: String) -> String
     {
-    	return "I'm Batman. Be caferul with ur words!"
+    	return phrase
     }
 }
 
@@ -13,4 +13,4 @@ print("Who are you?: ", terminator: "")
 let name = readLine()!
 let batman = Batman()
 print("Ok, \(name)!")
-print(batman.talk())
+print(batman.talk(by: "I'm Batman. Be caferul with ur words!"))
