eda5f6fd36e296ecd7c09587b3b410cb94faaa69
Guilherme Siani <guilherme@siani.com.br>
2016-11-10 23:49:54 -0200

    New class batman

--- Main.swift
+++ Main.swift
@@ -1,3 +1,16 @@
-print("enter your name: ", terminator: "")
+class Batman
+{
+	var life = 100;
+    var armor = 0;
+   
+    func talk() -> String
+    {
+    	return "I'm Batman. Be caferul with ur words!"
+    }
+}
+
+print("Who are you?: ", terminator: "")
 let name = readLine()!
-print("\(name)")
+let batman = Batman()
+print("Ok, \(name)!")
+print(batman.talk())
