49fdedca0e3ccc1e073badbcaa955b8832da9c0a
Guilherme Siani <guilherme@siani.com.br>
2016-11-15 23:42:13 -0200

    fix import Teste.swift

--- sources/main.swift
+++ sources/main.swift
@@ -16,3 +16,5 @@ let name = readLine()!
 let batman = Batman()
 print("Ok, \(name)!")
 print(batman.talk(by: "I'm Batman. Be caferul with ur words!"))
+let systempc = SystemPC()
+print(systempc.say(by: "Teste de sistema"))
