a8d4bac9fdb34ceea8818e8bc1bed4006354a0f0
Guilherme Siani <guilherme@siani.com.br>
2016-11-15 23:37:08 -0200

    Restore project to multi plataform version

---
+++ sources/Teste.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+class SystemPC
+{
+    func say(by phrase: String) -> String
+    {
+        return phrase
+    }
+}
---
+++ sources/main.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class Batman
+{
+	var life = 100;
+    var armor = 0;
+   
+    func talk(by phrase: String) -> String
+    {
+    	return phrase
+    }
+}
+
+print("Who are you?: ", terminator: "")
+let name = readLine()!
+let batman = Batman()
+print("Ok, \(name)!")
+print(batman.talk(by: "I'm Batman. Be caferul with ur words!"))
